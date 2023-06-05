14911e1d0bc697645df8fa1665d834408a25b979
Fome-a <ofomearubs@gmail.com>
2023-06-05 11:30:11 +0100

    tasks

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Template for API authentication
+"""
+from flask import request
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        return None
+
+    def current_user(self, request=None) -> Typevar('User'):
+        return None
