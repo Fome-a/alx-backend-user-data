ac6542f5be2c49052af0e2baac764a763b9b5b2e
Fome-a <ofomearubs@gmail.com>
2023-06-05 10:58:45 +0100

    tasks

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,7 +5,6 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -26,7 +25,8 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route("/unauthorized", methods=['GET'], strict_slashes=False)
 def unauthorised_endpoint():
-    """abort with 401 error"""    
+    """abort with 401 error"""
     abort(401)
