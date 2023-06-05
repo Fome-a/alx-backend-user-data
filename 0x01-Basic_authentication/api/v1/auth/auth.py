eeb2ca482a8347f470419252b849c83242acbd21
Fome-a <ofomearubs@gmail.com>
2023-06-05 11:37:38 +0100

    tasks

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,14 +3,15 @@
 Template for API authentication
 """
 from flask import request
+from typing import List, TypeVar
 
 
 class Auth:
-    def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
 
     def authorization_header(self, request=None) -> str:
         return None
 
-    def current_user(self, request=None) -> Typevar('User'):
+    def current_user(self, request=None) -> TypeVar('User'):
         return None
