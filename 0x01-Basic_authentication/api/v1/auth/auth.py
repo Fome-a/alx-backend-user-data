858e2fb492caa524e0a03c63c248e66801876bd0
Fome-a <ofomearubs@gmail.com>
2023-06-05 11:55:17 +0100

    tasks

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,11 +7,15 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """Manage the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require authorisation"""
         return False
 
     def authorization_header(self, request=None) -> str:
+        """authorisation user"""
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """current user"""
         return None
