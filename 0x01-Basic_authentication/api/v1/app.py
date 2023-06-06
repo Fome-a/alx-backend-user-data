381281625b0276e4411fbca08260a759504779a9
Fome-a <ofomearubs@gmail.com>
2023-06-06 11:51:34 +0100

    tasks

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -32,12 +32,15 @@ def before_request_handler() -> str:
         '/api/v1/forbidden/'
     ]
 
-    if not auth.require_auth(request.path, excluded_paths):
+    if request.path not in excluded_paths:
+        auth.require_auth(request.path, excluded_paths)
         return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    print(request.headers)
+    print(auth.authorization_header(request))
 
 
 @app.errorhandler(404)
