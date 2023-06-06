53fc28dfd452c3fce9abcc70ff3816c13b9e551c
Fome-a <ofomearubs@gmail.com>
2023-06-06 11:12:32 +0100

    tasks

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,13 +22,15 @@ class Auth:
                 return False
             else:
                 return True
-
+            
+        
     def authorization_header(self, request=None) -> str:
-        """authorisation user"""
-        if request is None or ('Authorization') not in request.headers.get:
+        """authorization header"""
+        if request is None or 'Authorization' not in request.headers:
             return None
         else:
             return request.headers.get('Authorization')
+
 
     def current_user(self, request=None) -> TypeVar('User'):
         """current user"""
