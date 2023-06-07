502b6412953b689a144183fdda69b94d86023724
Fome-a <ofomearubs@gmail.com>
2023-06-07 11:57:48 +0100

    tasks

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""" A user model """
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class User(Base):
+    """table of users with attributes specified"""   # creating a class User taht inherits from Base
+    __tablename__ = 'users'  # name of table
+
+    id = Column(Integer, primary_key=True)
+    email = Column(nullable=False)  # This means no row can be left null
+    hashed_password = Column(nullable=False)
+    session_id = Column(nullable=True)  # This means rows can be left null
+    reset_token = Column(nullable=True)
