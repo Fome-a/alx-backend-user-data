c5f78c4323b43bbeb420f94443f294f648683358
Fome-a <ofomearubs@gmail.com>
2023-06-09 05:43:17 +0100

    added user model import

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,9 +4,12 @@
 import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> str:
+    """takes in a password string, coverts it to bytes
+    salt hasshes the bytes with bcrypt.hashpw """
     bytes = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(bytes, bcrypt.gensalt())
     return hashed_password
@@ -23,6 +26,7 @@ class Auth:
         # and also use the database functions
 
     def register_user(self, email: str, password: str) -> str:
+        """Registers a new user to the database"""
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
