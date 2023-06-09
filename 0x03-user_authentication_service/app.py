be26366978eafca8893ae2d678f9286f20ac9715
Fome-a <ofomearubs@gmail.com>
2023-06-09 13:09:56 +0100

    added debug mode

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Basic Flask application"""
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 app = Flask(__name__)
+AUTH = Auth()  # this instatiates the Auth class and creates the object AUTH
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -12,5 +13,21 @@ def home():
     return respond
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
+    app.debug = True 
     app.run(host="0.0.0.0", port="5000")
+
+
+
